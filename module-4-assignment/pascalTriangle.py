b11aebbedeb283ffef672675aff1555277f96f5f
Md Mamun <mamun@smartfieldservice.com>
2025-02-11 15:44:31 +0600

    module4 assignment problem-01 solve

--- module-4-assignment/pascalTriangle.py
+++ module-4-assignment/pascalTriangle.py
@@ -1,1 +1,14 @@
-print("Array pascal triangle problem")
+def pascalTriangle(rows):
+    triangle = []
+    for i in range(rows):
+        row = [1] * (i+1)
+        for j in range(1, i):
+            row[j] = triangle[ i - 1][j - 1] + triangle[i-1][j]
+        triangle.append(row)
+    return triangle
+
+numRows = 5
+result = pascalTriangle(numRows)
+print(result)
+for row in result:
+    print(" ".join(map(str, row)).center(numRows * 4))
